e53aa9486b1023a92847ffbd563b5143c6b399c9
Felipe Galvao <contato@felipegalvao.com.br>
2017-01-27 12:21:35 -0200

    Add test suite for list component

---
+++ app/tests/components/TodoList.test.jsx
@@ -0,0 +1,31 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('../../../node_modules/jquery/dist/jquery.js');
+var TestUtils = require('react-addons-test-utils');
+
+var TodoList = require('TodoList')
+var Todo = require('Todo')
+
+describe('TodoList', () => {
+    it('should exist', () => {
+        expect(TodoList).toExist()
+    })
+
+    it('should render one Todo component for each Todo item', () => {
+        var todos = [
+            {
+                id: 1,
+                text: 'Do something'
+            },
+            {
+                id: 2,
+                text: 'Check mail'
+            }
+        ]
+        var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>)
+        var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo)
+
+        expect(todosComponents.length).toBe(todos.length)
+    })
+})
