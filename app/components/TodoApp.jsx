05951bff78986eebb9c79da181e143abda259700
Felipe Galvao <contato@felipegalvao.com.br>
2017-01-27 15:51:56 -0200

    Create todos search component

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,13 @@
 var React = require('react')
 var TodoList = require('TodoList')
 var AddTodo = require('AddTodo')
+var TodoSearch = require('TodoSearch')
 
 var TodoApp = React.createClass({
     getInitialState: function() {
         return {
+            showCompleted: false,
+            searchText: '',
             todos: [
                 {
                     id: 1,
@@ -26,11 +29,19 @@ var TodoApp = React.createClass({
         alert('new todo: ' + text)
     },
 
+    handleSearch: function(showCompleted, searchText) {
+        this.setState({
+            showCompleted: showCompleted,
+            searchText: searchText.toLowerCase()
+        })
+    },
+
     render: function () {
         var {todos} = this.state;        
 
         return (
             <div>
+                <TodoSearch onSearch={this.handleSearch} />
                 <TodoList todos={todos} />
                 <AddTodo onAddTodo={this.handleAddTodo} />
             </div>
