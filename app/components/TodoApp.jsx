8da2aae013cb717472aef05b52fab565d5924dcc
Felipe Galvao <contato@felipegalvao.com.br>
2017-01-30 14:02:12 -0200

    Add local storage for the todos

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,35 +1,24 @@
-var React = require('react')
-var TodoList = require('TodoList')
-var AddTodo = require('AddTodo')
-var TodoSearch = require('TodoSearch')
-var uuid = require('node-uuid')
+var React = require('react');
+var uuid = require('node-uuid');
+
+var TodoList = require('TodoList');
+var AddTodo = require('AddTodo');
+var TodoSearch = require('TodoSearch');
+var TodoAPI = require('TodoAPI');
 
 var TodoApp = React.createClass({
     getInitialState: function() {
         return {
             showCompleted: false,
             searchText: '',
-            todos: [
-                {
-                    id: uuid(),
-                    text: 'Walk the dog',
-                    completed: false
-                }, {
-                    id: uuid(),
-                    text: 'Clean the yard',
-                    completed: true
-                }, {
-                    id: uuid(),
-                    text: 'Buy potatos',
-                    completed: true
-                }, {
-                    id: uuid(),
-                    text: 'Cook the potatos',
-                    completed: false
-                }
-            ]
+            todos: TodoAPI.getTodos()
         }
     },
+
+    componentDidUpdate: function () {
+        TodoAPI.setTodos(this.state.todos);
+    },
+
     handleAddTodo: function(text) {
         this.setState({
             todos: [
