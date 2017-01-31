1bd8f2ad1db92508a1dadb3e6e0215c8879688a6
Felipe Galvao <contato@felipegalvao.com.br>
2017-01-31 01:10:03 -0200

    Complete filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -19,5 +19,40 @@ module.exports = {
         }
 
         return $.isArray(todos) ? todos : [];
+    },
+
+    filterTodos: function(todos, showCompleted, searchText) {
+        var filteredTodos = todos;
+
+        // Filter by showCompleted
+        filteredTodos = filteredTodos.filter((todo) => {
+            return !todo.completed || showCompleted;
+        })
+
+        // Filter by searchText
+        filteredTodos = filteredTodos.filter((todo) => {
+            if (searchText === '') {
+                return true;
+            } else {
+                if (todo.text.toLowerCase().indexOf(searchText) === -1) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }            
+        })
+
+        // Sort todos with non-completed first
+        filteredTodos.sort((a, b) => {
+            if (!a.completed && b.completed) {
+                return -1;
+            } else if (a.completed && !b.completed) {
+                return 1;
+            } else {
+                return 0;
+            }
+        })
+
+        return filteredTodos;
     }
 }
--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -46,11 +46,56 @@ describe('TodoAPI', () => {
                 text: "dummy text",
                 completed: false
             }];
-            
+
             localStorage.setItem('todos', JSON.stringify(todos));
             var actualTodos = TodoAPI.getTodos();
 
             expect(actualTodos).toEqual(todos);
-        })
+        });
     });
+
+    describe('filteredTodos', () => {
+        var todos = [
+            {
+                id: 1,
+                text: 'some text here',
+                completed: true
+            },
+            {
+                id: 2,
+                text: 'Other text here',
+                completed: false
+            },
+            {
+                id: 3,
+                text: 'some text here',
+                completed: true
+            }
+        ];
+
+        it('should return all items if showCompleted is true', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filteredTodos.length).toBe(3);
+        });
+
+        it('should return only non-completed items if showCompleted is false', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+            expect(filteredTodos.length).toBe(1);
+        });
+
+        it('should sort by completed status', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, '')
+            expect(filteredTodos[0].completed).toBe(false);
+        })
+
+        it('should filter todos by search text', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
+            expect(filteredTodos.length).toBe(2);
+        });
+
+        it('should return all todos if search text is empty', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filteredTodos.length).toBe(3);
+        });
+    })
 })
