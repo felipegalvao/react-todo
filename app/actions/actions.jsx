b9f7d6b0ff56bd534864f196b288a1975075fc77
Felipe Galvao <contato@felipegalvao.com.br>
2017-02-01 19:13:17 -0200

    Add actions & action test

---
+++ app/actions/actions.jsx
@@ -0,0 +1,28 @@
+export var setSearchText = (searchText) => {
+  return {
+    type: 'SET_SEARCH_TEXT',
+    searchText
+  }
+};
+
+//toggleShowCompleted action
+export var toggleShowCompleted = () => {
+  return {
+    type: 'TOGGLE_SHOW_COMPLETED'
+  };
+};
+
+export var addTodo = (text) => {
+  return {
+    type: 'ADD_TODO',
+    text
+  }
+}
+
+// toggleTodo(id)
+export var toggleTodo = (id) => {
+  return {
+    type: 'TOGGLE_TODO',
+    id
+  }
+}
